3e9e3edc3f7a66f89b25295fa9d15fe76423c28a
karthykarthick <karthylock@gmail.com>
2020-08-26 20:57:04 +0530

    remove linters errors

--- config.ru
+++ config.ru
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # config.ru
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
--- ruby_bot.rb
+++ ruby_bot.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'slack-ruby-bot'
 require './ruby-bot/bot'
 require './ruby-bot/commands/get_episodes'
