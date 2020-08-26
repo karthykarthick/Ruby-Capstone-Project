cd5fef31cfc5c401a8daf204fb8e4acae7bfc7bd
karthykarthick <karthylock@gmail.com>
2020-08-27 04:09:43 +0530

    remove unnecessary code

--- spec/slack_bot_spec.rb
+++ spec/slack_bot_spec.rb
@@ -2,7 +2,12 @@ require_relative '../ruby_bot'
 require_relative '../ruby-bot/commands/get_episodes'
 
 describe 'HelloText' do
+    let(:case1) {'hello'}
   it 'checks method' do
     expect(HelloText.say_hello).to eql('Hello! This is a Bot!')
+  end
+
+  it 'checks method' do
+    expect(HelloText.say_hello(case1)).to eql('Hello')
   end
 end
