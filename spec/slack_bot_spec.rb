9c66f7e8dffcdc4b749c92018a4ff20a8bdbb402
karthykarthick <karthylock@gmail.com>
2020-08-27 04:13:05 +0530

    add token

--- spec/slack_bot_spec.rb
+++ spec/slack_bot_spec.rb
@@ -2,7 +2,7 @@ require_relative '../ruby_bot'
 require_relative '../ruby-bot/commands/get_episodes'
 
 describe 'HelloText' do
-    let(:case1) {'hello'}
+  let(:case1) { 'hello' }
   it 'checks method' do
     expect(HelloText.say_hello).to eql('Hello! This is a Bot!')
   end
